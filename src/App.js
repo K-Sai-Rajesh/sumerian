fead538956d85a2f05a545c8871df3bf6e66de02
K-Sai-Rajesh <sairajeshk17@gmail.com>
2022-10-07 17:59:30 +0530

    sumerian update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/**
+ * @module threejs/HOST
+ */
 
-export default App;
+ import {
+    Utils,
+    MathUtils,
+    Deferred,
+    LipsyncFeature,
+    DefaultVisemeMap,
+    GestureFeature,
+    DefaultGestureWords,
+    Messenger,
+  } from '@amazon-sumerian-hosts/core';
+  import PointOfInterestFeature, {AxisMap} from './PointOfInterestFeature';
+  import {env} from './HostEnvironment';
+  import HostObject from './HostObject';
+  
+  import aws from './awspack';
+  import anim from './animpack';
+  
+  export {
+    /**
+     * @see module:core/HOST.Utils
+     */
+    Utils,
+    /**
+     * @see module:core/HOST.MathUtils
+     */
+    MathUtils,
+    /**
+     * @see module:core/HOST.Deferred
+     */
+    Deferred,
+    /**
+     * @see module:core/HOST.LipsyncFeature
+     */
+    LipsyncFeature,
+    /**
+     * @see module:core/HOST.GestureFeature
+     */
+    GestureFeature,
+    /**
+     * @see module:core/HOST.DefaultVisemeMap
+     */
+    DefaultVisemeMap,
+    /**
+     * @see module:core/HOST.DefaultGestureWords
+     */
+    DefaultGestureWords,
+    /**
+     * @see threejs/PointOfInterestFeature
+     */
+    PointOfInterestFeature,
+    /**
+     * @see env
+     */
+    env,
+    /**
+     * @see module:core/Messenger
+     */
+    Messenger,
+    /**
+     * @see threejs/HostObject
+     */
+    HostObject,
+    /**
+     * @see AxisMap
+     */
+    AxisMap,
+    /**
+     * @see module:threejs/awspack
+     */
+    aws,
+    /**
+     * @see module:threejs/animpack
+     */
+    anim,
+  };
+  
--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/**
+ * @module threejs/HOST
+ */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import {
+  Utils,
+  MathUtils,
+  Deferred,
+  LipsyncFeature,
+  DefaultVisemeMap,
+  GestureFeature,
+  DefaultGestureWords,
+  Messenger,
+} from '@amazon-sumerian-hosts/core';
+import PointOfInterestFeature, {AxisMap} from './PointOfInterestFeature';
+import {env} from './HostEnvironment';
+import HostObject from './HostObject';
+
+import aws from './awspack';
+import anim from './animpack';
+
+export {
+  /**
+   * @see module:core/HOST.Utils
+   */
+  Utils,
+  /**
+   * @see module:core/HOST.MathUtils
+   */
+  MathUtils,
+  /**
+   * @see module:core/HOST.Deferred
+   */
+  Deferred,
+  /**
+   * @see module:core/HOST.LipsyncFeature
+   */
+  LipsyncFeature,
+  /**
+   * @see module:core/HOST.GestureFeature
+   */
+  GestureFeature,
+  /**
+   * @see module:core/HOST.DefaultVisemeMap
+   */
+  DefaultVisemeMap,
+  /**
+   * @see module:core/HOST.DefaultGestureWords
+   */
+  DefaultGestureWords,
+  /**
+   * @see threejs/PointOfInterestFeature
+   */
+  PointOfInterestFeature,
+  /**
+   * @see env
+   */
+  env,
+  /**
+   * @see module:core/Messenger
+   */
+  Messenger,
+  /**
+   * @see threejs/HostObject
+   */
+  HostObject,
+  /**
+   * @see AxisMap
+   */
+  AxisMap,
+  /**
+   * @see module:threejs/awspack
+   */
+  aws,
+  /**
+   * @see module:threejs/animpack
+   */
+  anim,
+};
